b3360ce13f641c3e966155c77eee6596856c0d53
Rouan Thompson <rouan.thompson@gmail.com>
2020-06-05 15:36:11 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,10 @@
 # don't forget to add: require 'pry'
 
 def snake_it_up(string)
-  if string[0] == "s" do 10.times
-    print 's'
-  end
+  if string[0] == "s"
+    do 10.times
+      print 's'
+    end
   else
     string
   end
